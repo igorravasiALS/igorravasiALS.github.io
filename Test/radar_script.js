4f42e8790cf4688f5c0e8db99140ec5b9e0ae1f2
alessandronoceraals <127126097+alessandronoceraals@users.noreply.github.com>
2024-07-30 18:36:38 +0200

    Update radar_script.js

--- Test/radar_script.js
+++ Test/radar_script.js
@@ -12,7 +12,7 @@
 		  var i, LegendOptions, colordata, row, color, d;
 		  
 		  // funzione per estrarre un array contenente le dimensioni per i poligoni
-		  function getLegend(data){
+		  function getLegend(data) => {
 			var legend = [];
 			for(i in data) legend[i]=data[i][3].formattedValue;
 	
